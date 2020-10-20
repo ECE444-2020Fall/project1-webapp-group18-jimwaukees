91dcc11163053d1e15adcdd1200b061e4c6d41ca
Devesh Nischal <devesh.nischal@gmail.com>
2020-10-20 00:34:29 -0400

    Added initial component logic including search input and search dropdown. Also added about us and help tabs which will be filled out at a later date. Waiting for backend changes to connect the newly added components.

--- ui-react/src/App.js
+++ ui-react/src/App.js
@@ -1,10 +1,17 @@
 import React, {useEffect, useState} from 'react';
+import { Menu } from 'semantic-ui-react'
 import './App.css';
+import { SearchRecipes } from './components/SearchRecipes'
+import { SearchIngredients } from './components/SearchIngredients'
+import { About } from './components/About'
+import { Help } from './components/Help'
+import image from './Placeholder_Icon.png'
 
 function App() {
   const [recipeCount, setRecipeCount] = useState(0);
   const [recipes, setRecipes] = useState([]);
   const [specialRecipe, setSpecialRecipe] = useState('');
+  const [activeTab, setActiveTab] = useState('search_recipes');
 
   useEffect(() => {
     fetch("/get_recipes").then(response => {
@@ -21,24 +28,42 @@ function App() {
     });
   }, []);
 
+const handleTabClick = (e, { name }) => {
+  setActiveTab(name);
+};
+
   return (
     <div className="App">
+      <Menu pointing secondary>
+        <Menu.Item>
+          <img src={image} />
+        </Menu.Item>
+        <Menu.Item
+          name='search_recipes'
+          active={activeTab === 'search_recipes'}
+          onClick={handleTabClick}
+        />
+        <Menu.Item
+          name='search_ingredients'
+          active={activeTab === 'search_ingredients'}
+          onClick={handleTabClick}
+        />
+        <Menu.Item
+          name='about_us'
+          active={activeTab === 'about_us'}
+          onClick={handleTabClick}
+        />
+        <Menu.Item
+          name='help'
+          active={activeTab === 'help'}
+          onClick={handleTabClick}
+        />
+      </Menu>
       <header className="App-header">
-        <div>
-          Recipe you searched for: {specialRecipe}
-        </div>
-        <div>
-          List of recipes({recipeCount}):
-        </div>
-        {
-          recipes.map(recipe => { 
-              return (
-                <div>
-                  {recipe.name} 
-                </div>
-              )
-          })
-        }
+        {activeTab === 'search_recipes' ? <SearchRecipes searchedRecipe={specialRecipe} /> : <></>}
+        {activeTab === 'search_ingredients' ? <SearchIngredients recipeCount={recipeCount} recipes={recipes} /> : <></>}
+        {activeTab === 'about_us' ? <About /> : <></>}
+        {activeTab === 'help' ? <Help /> : <></>}
       </header>
     </div>
   );
---
+++ ui-react/src/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export function About() {
+    return (
+        <>
+            <div>
+                Sophia Ryoo
+            </div>
+            <div>
+                Matthew Viegas
+            </div>
+            <div>
+                Chris Pua
+            </div>
+            <div>
+                Devesh Nischal
+            </div>
+        </>
+    );
+}
