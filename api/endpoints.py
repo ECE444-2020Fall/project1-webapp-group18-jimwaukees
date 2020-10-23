0a35291b210d4d6a03fb3683c1a4fd34d6932fb6
Matthew Viegas <39061064+matthewviegas@users.noreply.github.com>
2020-10-23 12:06:11 -0400

    Implemented api query for ingredients based search

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,7 +1,7 @@
 import config
 
 from flask import Blueprint, jsonify
-from flask import request 
+from flask import request
 import requests
 
 main = Blueprint('main', __name__)
@@ -13,22 +13,55 @@ def get_recipe():
 
 @main.route('/get_recipes', methods=['GET'])
 def get_recipes():
-    recipes = [
-        {'name': 'burger'},
-        {'name': 'chicken'},
-        {'name': 'pork'},
-        {'name': 'fish'},
-        {'name': 'noodles'},
-    ]
-    return jsonify({'count': len(recipes), 'recipes': recipes})
-  
-  
+    count = 0
+    ingredients = list()
+
+    while request.args.get('ing' + str(count)) != None:
+        ingredients.append(request.args.get('ing' + str(count), type = str))
+        count += 1
+
+    if ingredients:
+        #ingredients: List of ingredients user searched for
+        #number: Nmber of recipes for the search to return
+        #ranking: Whether to maximize used ingredients (1) or minimize missing
+        #ingredients (2) first
+        #ignorePantry: Whether to ignore typical pantry items, such as water, salt, flour, etc.
+        #apiKey: Always need an API key or else you can't make the call to the API
+        params = {
+            'ingredients': ingredients,
+            'number' : 10,
+            'ranking' : 2,
+            'ignorePantry' : 'true',
+            'apiKey' : config.api_key,
+        }
+
+        recipes =  requests.get(
+            'https://api.spoonacular.com/recipes/findByIngredients',
+            params = params
+        ).json()
+
+        recipe_instruction = list()
+        params = {
+            'apiKey' : config.api_key
+        }
+
+        for i in range(len(recipes)):
+            instructions = requests.get(
+                "https://api.spoonacular.com/recipes/" \
+                + str(recipes[i].get('id')) + "/analyzedInstructions",
+                params = params
+            )
+            recipe_instruction.append((recipes[i], instructions.json()))
+
+        return jsonify({'recipeInstructions': recipe_instruction})
+
+
 @main.route('/get_spoontacular_recipes', methods=['GET'])
 def get_spoontacular_recipes():
   # Retrieve recipe name from URL param
   # recipe_query = request.args.get('recipe')
   recipe_query = 'pasta'
-  
+
   # apiKey: Always need an API key or else you can't make the call to the API
   # query: Name of recipe to be searched for. Returns results with query string included in the recipe title
   # number: Returns the number of recipes. JSON response include total number of results from query
@@ -37,13 +70,13 @@ def get_spoontacular_recipes():
     'query': recipe_query,
     'number': 20,
   }
-  
+
   print(f'\n\n\nThese are my params: {params}\n\n\n')
-  
+
   # Calling spoontacular API to search for a specific recipe
   response = requests.get(
     'https://api.spoonacular.com/recipes/complexSearch',
     params=params
   )
-  
+
   return response.json()
