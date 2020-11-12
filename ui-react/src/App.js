0110b4f8c3b2d1dc345b6493d95591a98001d20e
Devesh Nischal <devesh.nischal@gmail.com>
2020-11-12 00:42:33 -0500

    Connected recipe cards with each search page so search results can now be seen in card form. Additionally, when user clicks on a card, the respective recipe details popup shows up.

--- ui-react/src/App.js
+++ ui-react/src/App.js
@@ -6,31 +6,24 @@ import { SearchIngredients } from './components/SearchIngredients'
 import { About } from './components/About'
 import { Help } from './components/Help'
 import image from './ezcook.png'
-import Card from './components/Card'
-import { Grid } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles({
-  gridContainer: {
-    paddingLeft: '20px',
-    paddingRight: '20px',
-    paddingTop: '20px'
-
-  }
-
-
-});
 
 function App() {
-    const [recipeCount, setRecipeCount] = useState(0);
-    const [recipes, setRecipes] = useState([]);
-    const [specialRecipe, setSpecialRecipe] = useState('');
     const [activeTab, setActiveTab] = useState('search_recipes');
+    const [prevIngList, setPrevIngList] = useState([]);
+    const [recipeResultsIng, setRecipeResultsIng] = useState({
+        recipes: []
+    });
+    const [prevSearch, setPrevSearch] = useState('');
+    const [recipeResults, setRecipeResults] = useState({
+        number: 0,
+        offset: 0,
+        results: []
+    });
 
     // ? Chris: I think having specialRecipe is kind of redundant since we can get the input directly from the SearchRecipes.js
     // ? I think if we just pass in setRecipes to set the new recipe list into that component, there's no need for the other useState
     useEffect(() => {
-        fetch('/get_recipes').then(response => {
+        /*fetch('/get_recipes').then(response => {
             response.json().then(data => {
                 setRecipeCount(data.count);
                 setRecipes(data.recipes);
@@ -41,77 +34,62 @@ function App() {
             response.json().then(data => {
                 setSpecialRecipe(data.name);
             });
-        });
+        });*/
     }, []);
 
     const handleTabClick = (e, { name }) => {
         setActiveTab(name);
     };
 
-const gridClass = useStyles();
-  return (
-    
-    <div className="App">
-      <Menu pointing secondary>
-        <Menu.Item data-testid="company_logo">
-          <img src={image} />
-        </Menu.Item>
-        <Menu.Item
-          name='search_recipes'
-          active={activeTab === 'search_recipes'}
-          onClick={handleTabClick}
-        />
-        <Menu.Item
-          name='search_ingredients'
-          active={activeTab === 'search_ingredients'}
-          onClick={handleTabClick}
-        />
-        <Menu.Item
-          name='about_us'
-          active={activeTab === 'about_us'}
-          onClick={handleTabClick}
-        />
-        <Menu.Item
-          name='help'
-          active={activeTab === 'help'}
-          onClick={handleTabClick}
-        />
-      </Menu>
-      <header className="App-header">
-        {activeTab === 'search_recipes' ? <SearchRecipes setRecipes={setRecipes} /> : <></>}
-        {activeTab === 'search_ingredients' ? <SearchIngredients recipeCount={recipeCount} recipes={recipes} /> : <></>}
-        {activeTab === 'about_us' ? <About /> : <></>}
-        {activeTab === 'help' ? <Help /> : <></>}
-      </header>
-      <Grid container spacing ={4} className = { gridClass.gridContainer }>
-      <Grid item xs={12} sm = {6} md = {4}>
-      <Card />
-      </Grid> 
-
-      <Grid item xs={12} sm = {6} md = {4}>
-      <Card />
-      </Grid> 
-
-      <Grid item xs={12} sm = {6} md = {4}>
-      <Card />
-      </Grid> 
-
-      <Grid item xs={12} sm = {6} md = {4}>
-      <Card />
-      </Grid> 
-
-      <Grid item xs={12} sm = {6} md = {4}>
-      <Card />
-      </Grid> 
-
-      <Grid item xs={12} sm = {6} md = {4}>
-      <Card />
-      </Grid> 
-
-      </Grid>
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Menu pointing secondary>
+                <Menu.Item data-testid="company_logo">
+                    <img src={image} />
+                </Menu.Item>
+                <Menu.Item
+                    name='search_recipes'
+                    active={activeTab === 'search_recipes'}
+                    onClick={handleTabClick}
+                />
+                <Menu.Item
+                    name='search_ingredients'
+                    active={activeTab === 'search_ingredients'}
+                    onClick={handleTabClick}
+                />
+                <Menu.Item
+                    name='about_us'
+                    active={activeTab === 'about_us'}
+                    onClick={handleTabClick}
+                />
+                <Menu.Item
+                    name='help'
+                    active={activeTab === 'help'}
+                    onClick={handleTabClick}
+                />
+            </Menu>
+            <header className="App-header">
+                {activeTab === 'search_recipes' ? 
+                    <SearchRecipes 
+                        prevSearch={prevSearch}
+                        setPrevSearch={setPrevSearch}
+                        recipeResults={recipeResults}
+                        setRecipeResults={setRecipeResults}
+                    /> : 
+                <></>}
+                {activeTab === 'search_ingredients' ? 
+                    <SearchIngredients 
+                        prevIngList={prevIngList}
+                        setPrevIngList={setPrevIngList}
+                        recipeResults={recipeResultsIng}
+                        setRecipeResults={setRecipeResultsIng}
+                    /> : 
+                <></>}
+                {activeTab === 'about_us' ? <About /> : <></>}
+                {activeTab === 'help' ? <Help /> : <></>}
+            </header>
+        </div>
+    );
 }
 
 export default App;
